a94cf584e2d81aa0e82a1b2805e3e01efe322c1f
Rebeca Reis <118316578+RebecaSilva25@users.noreply.github.com>
2023-08-14 22:52:16 -0300

    Add files via upload

---
+++ src/caixa-da-lanchonete.js
@@ -0,0 +1,22 @@
+
+cardapio = {
+    'cafe': ('Café', 3.00),
+    'chantily': ('Chantily (extra do Café)', 1.50),
+    'suco': ('Suco Natural', 6.20),
+    'sanduiche': ('Sanduíche', 6.50),
+    'queijo': ('Queijo (extra do Sanduíche)', 2.00),
+    'salgado': ('Salgado', 7.25),
+    'combo1': ('1 Suco e 1 Sanduíche', 9.50),
+    'combo2': ('1 Café e 1 Sanduíche', 7.50)
+}
+- Pagamento em dinheiro tem 5% de desconto
+- Pagamento a crédito tem acréscimo de 3% no valor total
+class CaixaDaLanchonete {
+
+    calcularValorDaCompra(dinheiro, café) {
+        return "total= 2,85";
+        return "total";
+
+}
+}
+export { CaixaDaLanchonete };
